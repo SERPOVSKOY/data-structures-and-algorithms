19a25d1351bb155e48fb36d9476eb1d41e847f14
Alexey Serpovskoy <SERPOVSKOY.96@mail.ru>
2021-04-25 16:38:58 +0300

    added binary search function

---
+++ 02_binary_search.js
@@ -0,0 +1,30 @@
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+/**
+ * Функция(алгоритм) бинарного поиска элемента в массиве.
+ * Данныая функция работает с отсортированными массивами.
+ *
+ * @param {Array} array массив поиска
+ * @param {number} value искомы элемент
+ * @return {number} если значение найдено возвращает индекс элемента массива, иначе -1
+ */
+function binarySearch(array, value) {
+    let left = 0;
+    let right = array.length - 1;
+    let mid;
+
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+
+        if (array[mid] === value) {
+            return mid;
+        } else if (value < array[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+console.log('Индекс', binarySearch(array, 5));
